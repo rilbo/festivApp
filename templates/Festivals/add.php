06f4afd737b4ca3ab56ac2f5635c5852b25979c4
valentin <valmuller14@gmail.com>
2022-08-10 00:02:04 +0200

    [style] Festivals

--- templates/Festivals/add.php
+++ templates/Festivals/add.php
@@ -1,16 +1,23 @@
-<div>
-<div class="preview festival-img">
+<div id="header-custom">
+    <div>
+        <h2>Ajout d'un festival</h2>
+        <p>@<?= $this->request->getAttribute('identity')->pseudo?>-Admin</p>
+    </div>
+    <div>
+        <a href="#" id="submit-festiv">Ajouter</a>
+    </div>
 </div>
-<div>
-    <?= $this->Form->create($festival, ['enctype'=>'multipart/form-data']);?>
-        <?= $this->Form->control('image', [ 'label' => 'Image', 'type' => 'file']); ?>
-        <?= $this->Form->control('title'); ?>
-        <?= $this->Form->control('content', [ 'label' => 'Description']); ?>
-        <?= $this->Form->control('place', [ 'label' => 'Lieux']); ?>
-        <?= $this->Form->control('link', [ 'label' => 'Lien du site internet officiel']); ?>
-        <?= $this->Form->button('Ajouter');?>
-    <?= $this->Form->end(); ?>
+<div id="add-festival-form">
+    <div class="preview">
+    </div>
+    <div>
+        <?= $this->Form->create($festival, ['enctype'=>'multipart/form-data','class' => 'addfestiv']);?>
+            <?= $this->Form->control('image', [ 'label' => 'Image', 'type' => 'file']); ?>
+            <?= $this->Form->control('title', [ 'label' => 'Titre']); ?>
+            <?= $this->Form->control('content', [ 'label' => 'Description']); ?>
+            <?= $this->Form->control('place', [ 'label' => 'Lieux']); ?>
+            <?= $this->Form->control('link', [ 'label' => 'Lien du site internet officiel']); ?>
+        <?= $this->Form->end(); ?>
+    </div>
 </div>
-    
-</div>
-<?= $this->Html->script(['preview']) ?>
+<?= $this->Html->script(['preview','submit']) ?>
--- templates/Festivals/view.php
+++ templates/Festivals/view.php
@@ -1,19 +1,27 @@
-<div id="header-custom">
+<div id="header-custom" class="header-festival-view">
     <div>
         <?= $this->Html->link($this->Html->image('icons/navbarTop/back.svg', ['alt' => 'retourner à la page navigation des posts']), ['controller' => 'Posts', 'action' => 'index'], ['escape' => false]); ?>
     </div>
     <div>
-        <h3><?= $festival->title ?></h3>
-        <p><?= $festival->place ?></p>
+        <h3>Festival</h3>
     </div>
 </div>
-<div class="img-festival">
-    <?= $this->Html->image('pictures/festivals/'.$festival->picture.'', ['alt' => 'photo du festival '.$festival->title.'', 'style' => 'width:100%;']); ?>
-</div>
-<div>
-    <p><?= $festival->content ?></p>
-</div>
-<div>
-    <h4>Lien du site</h4>
-    <?= $this->Html->link($festival->link, $festival->link, ['target' => '_blank', 'escape' => false]); ?>
-</div>
+<?php if ($festival->content == null) : ?>
+    <div id="content-custom">
+        <p>Aucun contenu pour ce festival</p>
+        <p>La page de présentation arrive prochainement</p>
+    </div>
+<?php else : ?>
+    <div class="img-festival">
+        <?= $this->Html->image('pictures/festivals/'.$festival->picture.'', ['alt' => 'photo du festival '.$festival->title.'', 'style' => 'width:100%;']); ?>
+    </div>
+    <div id="content-festival">
+        <h3><?= $festival->title ?></h3>
+        <p><?= $festival->place ?></p>
+        <p><?= $festival->content ?></p>
+    </div>
+    <div id="link-festival">
+        <h4>Lien du site</h4>
+        <?= $this->Html->link($festival->link, $festival->link, ['target' => '_blank', 'escape' => false]); ?>
+    </div>
+<?php endif; ?>
